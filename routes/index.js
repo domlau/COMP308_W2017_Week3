a1d2223dc44fc4013ab8c3c12e6fcead35385337
domlau <dominic.k.y.lau@gmail.com>
2017-01-28 16:42:24 -0500

    installed bower, bootstrap, jquery

--- routes/index.js
+++ routes/index.js
@@ -15,9 +15,8 @@ router.get('/', (req, res, next) => {
 
 /* GET about page. */
 router.get('/about', (req, res, next) => {
-  res.render('index', { 
+  res.render('about', { 
     title: 'About',
-    date: currentDate
   });
 });
 
