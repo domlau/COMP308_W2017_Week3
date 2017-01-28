bbdff745828ae67807cff571c3e4fa5597b6ee1e
domlau <dominic.k.y.lau@gmail.com>
2017-01-28 15:52:36 -0500

    migrated to Express and EJS

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+let express = require('express');
+let router = express.Router();
+//Local route variables  
+let currentDate = new Date();
+currentDate = currentDate.toLocaleString();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+
+  res.render('index', { 
+    title: 'Express',
+    date: currentDate
+  });
+});
+
+/* GET about page. */
+router.get('/about', (req, res, next) => {
+  res.render('index', { 
+    title: 'About',
+    date: currentDate
+  });
+});
+
+module.exports = router;
